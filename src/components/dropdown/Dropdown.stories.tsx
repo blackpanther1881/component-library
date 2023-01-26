ab620a917a0862f684de0c7e9d60cb807a21b1c1
blackpanther1881 <raju@persistence.one>
2023-01-26 10:33:06 +0400

    add tab component

--- src/components/dropdown/Dropdown.stories.tsx
+++ src/components/dropdown/Dropdown.stories.tsx
@@ -3,52 +3,69 @@ import React from "react";
 import { Dropdown, DropdownProps } from "./Dropdown";
 
 export default {
-    title: "Dropdown",
-    component: Dropdown,
-    decorators: [
-        (Story) => {
-            return (
-                <div className="w-80">
-                    <Story />
-                </div>
-            );
-        },
-    ],
+  title: "Dropdown",
+  component: Dropdown,
+  decorators: [
+    (Story) => {
+      return (
+        <div className="w-80">
+          <Story />
+        </div>
+      );
+    }
+  ]
 } as ComponentMeta<typeof Dropdown>;
 
 const Template: ComponentStory<typeof Dropdown> = (args) => {
-    return <Dropdown {...args} />;
+  return <Dropdown {...args} />;
 };
 
 export const BasicExample = Template.bind({});
 // @ts-ignore
 BasicExample.args = {
-    dropdownLabel: "Basic Dropdown",
-    contentList: [
-        {
-            optionName: "option1",
-        },
-        {
-            optionName: "option1",
-        }
-    ],
-    rounded: false
+  dropdownLabel: "Basic Dropdown",
+  contentList: [
+    {
+      optionName: "option1"
+    },
+    {
+      optionName: "option1"
+    }
+  ],
+  rounded: false
 } as DropdownProps;
 
 export const customDropdownButton = Template.bind({});
 // @ts-ignore
 customDropdownButton.args = {
-    dropdownLabel: "Custom Dropdown",
-    contentList: [
-        {
-            optionName: "option1",
-        },
-        {
-            optionName: "option1",
-        }
-    ],
-    dropDownVariant: "custom",
-    dropDownVariantBg: "bg-black-full text-light-high",
-    rounded: false
+  dropdownLabel: "Custom Dropdown",
+  contentList: [
+    {
+      optionName: "option1"
+    },
+    {
+      optionName: "option1"
+    }
+  ],
+  dropDownVariant: "custom",
+  dropDownVariantBg: "bg-black-full text-light-high",
+  rounded: false
 } as DropdownProps;
 
+export const hoverDropdownButton = Template.bind({});
+// @ts-ignore
+hoverDropdownButton.args = {
+  dropdownLabel: "Hover Dropdown",
+  contentList: [
+    {
+      optionName: "option1"
+    },
+    {
+      optionName: "option1"
+    }
+  ],
+  dropDownVariant: "custom",
+  DropdownType: "hover",
+  dropDownVariantBg: "bg-black-full text-light-high",
+  rounded: false
+} as DropdownProps;
--- src/components/dropdown/Dropdown.tsx
+++ src/components/dropdown/Dropdown.tsx
@@ -1,88 +1,92 @@
-import React, {useRef} from "react";
-import {
-    DetailedHTMLProps,
-    forwardRef,
-    HTMLAttributes,
-    useState,
-} from "react";
-import {Button} from "../button";
-import {useOnClickOutside} from "../../utils/useOnClickOutside";
+import React, { useRef } from "react";
+import { DetailedHTMLProps, forwardRef, HTMLAttributes, useState } from "react";
+import { Button } from "../button";
+import { useOnClickOutside } from "../../utils/useOnClickOutside";
 
 export interface optionList {
-    optionName: React.ReactNode | string;
-    handler?: () => void;
-    image?: React.ReactNode | string;
+  optionName: React.ReactNode | string;
+  handler?: () => void;
+  image?: React.ReactNode | string;
 }
+
 export type DropdownButtonVariants = "primary" | "custom";
+export type DropdownType = "hover" | "click";
 
 export type DropdownProps = {
-    dropdownLabel: React.ReactNode | string;
-    className?: string;
-    contentList: optionList[];
-    rounded?: boolean;
-    staticBackDrop?:boolean;
-    dropDownVariant: DropdownButtonVariants
-    dropDownVariantBg?: string
+  dropdownLabel: React.ReactNode | string;
+  DropdownType?: DropdownType;
+  className?: string;
+  contentList: optionList[];
+  rounded?: boolean;
+  staticBackDrop?: boolean;
+  dropDownVariant: DropdownButtonVariants;
+  dropDownVariantBg?: string;
 } & DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement>;
 
-
 export const Dropdown = forwardRef<HTMLDivElement, DropdownProps>(
-    ({ dropdownLabel, contentList,
-         className = "",
-         rounded = false,
-         staticBackDrop = true,
-         dropDownVariant = "primary", dropDownVariantBg = ""}) => {
-        const [open, setOpen] = useState<boolean>(false);
-        const topRounding = rounded ? "rounded-t-md" : "";
+  ({
+    dropdownLabel,
+    contentList,
+    className = "",
+    rounded = false,
+    staticBackDrop = true,
+    dropDownVariant = "primary",
+    dropDownVariantBg = "",
+    DropdownType = "click"
+  }) => {
+    const [open, setOpen] = useState<boolean>(false);
+    const topRounding = rounded ? "rounded-t-md" : "";
 
-        const ref = useRef<HTMLDivElement>(null);
+    const ref = useRef<HTMLDivElement>(null);
 
-        useOnClickOutside(ref, () => {
-            setOpen(false);
-        });
+    useOnClickOutside(ref, () => {
+      setOpen(false);
+    });
 
-        return (
-            <div
-                className={`${topRounding} ${className} text-white relative inline-block`}
-                ref={staticBackDrop ? null : ref}
-            >
-                <Button
-                    name={"buttonConnect"}
-                    className={`dropDownButton w-full md:py-2 md:text-sm`}
-                    variant={dropDownVariant === "primary" ? "solid": "custom" }
-                    scale="lg"
-                    isDisabled={false}
-                    onClick={()=>setOpen((prev) => !prev)}
-                    customButtonClass={dropDownVariant !== "primary" ?
-                        dropDownVariantBg ? `${dropDownVariantBg}`
-                         : "bg-black-800 text-light-high" :  ""
-                        }
-                >
-                    {dropdownLabel}
-                </Button>
-                <div
-                    className={`absolute opacity-0 transition-opacity transform ease duration-200 bg-dropDown left-0 right-0 ${
-                        open ? "visible translate-y-0 opacity-100" : "invisible"
-                    } text-light-high rounded-md`}
-                >
-                    {contentList.map((item: any, index: number) => (
-                        <div key={index}>
-                            <div
-                                className="px-4 py-2 flex items-center md:py-3"
-                                onClick={item.handler}
-                            >
-                                {item.image ? (
-                                    item.image
-                                ) : null}
-                                <span className="ml-4 text-light-high text-sm font-medium leading-normal md:text-xsm md:ml-2">
-                                    {item.optionName}
-                                </span>
-                            </div>
-                        </div>
-                    ))
-                    }
-                </div>
+    return (
+      <div
+        className={`${topRounding} ${className} text-white relative inline-block dropDown ${
+          DropdownType === "click" ? "dropDownClick" : "dropDownHover"
+        }`}
+        ref={staticBackDrop ? null : ref}
+      >
+        <Button
+          name={"buttonConnect"}
+          className={`dropDownButton w-full md:py-2 md:text-sm`}
+          variant={dropDownVariant === "primary" ? "solid" : "custom"}
+          scale="lg"
+          isDisabled={false}
+          onClick={() => setOpen((prev) => !prev)}
+          customButtonClass={
+            dropDownVariant !== "primary"
+              ? dropDownVariantBg
+                ? `${dropDownVariantBg}`
+                : "bg-black-800 text-light-high"
+              : ""
+          }
+        >
+          {dropdownLabel}
+        </Button>
+        <div
+          className={`dropDownContent absolute opacity-0 transition-opacity transform ease duration-200 bg-dropDown left-0 right-0 ${
+            open ? "visible translate-y-0 opacity-100" : "invisible"
+          } text-light-high rounded-md`}
+        >
+          {contentList.map((item: any, index: number) => (
+            <div key={index}>
+              <div
+                className="px-4 py-2 flex items-center md:py-3 hover:cursor-pointer"
+                onClick={item.handler}
+              >
+                {item.image ? item.image : null}
+                <span className="ml-4 text-light-high text-sm font-medium leading-normal md:text-xsm md:ml-2">
+                  {item.optionName}
+                </span>
+              </div>
             </div>
-        );
-    }
+          ))}
+        </div>
+      </div>
+    );
+  }
 );
